09f645f80aefb6006d37d2e14d0b5103cba01535
Priyanshu-sde <Priyanshu.sde@gmail.com>
2024-04-21 18:21:27 +0530

    New file

---
+++ A_Fox_And_Snake.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>n>>m;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j < m;j++){
+            if((i%2) && j == 0 && i%4 == 3)
+            cout<<"#";
+            if(!(i%2))
+            cout<<"#";
+            if(i%2 && j < m -1)
+            cout<<".";
+            if((i%2) && j == m-1 && i%4 == 1)
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+}
