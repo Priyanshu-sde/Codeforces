282666fcfc026ad06353251497e569bd8530d4c5
Priyanshu-sde <Priyanshu.sde@gmail.com>
2024-05-01 16:15:51 +0530

    new

---
+++ C_Target_Practice.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector <vector<int>> arr;
+        for(int  i = 0;i < 10;i++){
+            for(int j = 0; j < 10;j++){
+                cin>>arr[i][j];
+                if(arr[i][j] == 'X'){
+                    i
+                }
+            }
+        }
+
+    }
+}
---
+++ MISSSUMS.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int  t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int a = 1;
+	    for(int  i = 0; i < n;i++){
+	        cout<<a<<" ";
+	        a +=2;
+	    }
+	    cout<<endl;
+	    
+	}
+
+}
