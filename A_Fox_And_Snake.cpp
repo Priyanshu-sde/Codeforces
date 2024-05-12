ebff70f7d75866a749b83acc4cb03d5f430f9734
Priyanshu-sde <Priyanshu.sde@gmail.com>
2024-05-12 11:44:34 +0100

    Check

--- A_Fox_And_Snake.cpp
+++ A_Fox_And_Snake.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int m,n;
     cin>>n>>m;
-    for(int i = 0;i<n;i++){
+    for(int i = 0;i < n;i++){
         for(int j = 0;j < m;j++){
             if((i%2) && j == 0 && i%4 == 3)
             cout<<"#";
@@ -12,7 +12,7 @@ int main(){
             if(i%2 && j < m -1)
             cout<<".";
             if((i%2) && j == m-1 && i%4 == 1)
-            cout<<"#";
+            cout<<"*";
         }
         cout<<endl;
     }
