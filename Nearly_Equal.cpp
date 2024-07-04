fc9d31e8ddef73d712d37d68124d47bd0139e336
Priyanshu-sde <priyanshu.sde@gmail.com>
2024-07-04 21:58:41 +0530

    Done

---
+++ Nearly_Equal.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fr(i,n) for(int  i = 0;i < n;i++)
+#define sr(a) sort(a.begin(),a.end())
+#define srd(a) sort(a.begin(),a.end(),greater<int>())
+#define ll long long
+#define MOD = 10e7;
+int main(){
+    cout<<floor(5/2)<<endl;
+        
+    }
---
+++ Redundant_Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+        vector<int> a(n);
+        for(int i = 0 ;i < n;i++){
+            cin>>a[i];
+        }
+
+	    
+	}
+
+}
