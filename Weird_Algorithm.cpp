a6cbae7905586818f3a7a7a740e41160eaed906d
Priyanshu-sde <priyanshu.sde@gmail.com>
2024-08-08 13:40:00 +0530

    Done

--- Weird_Algorithm.cpp
+++ Weird_Algorithm.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 #define sr(a) sort(a.begin(),a.end())
-#define srd(a) sort(a.begin(),a.end(),greater<int>())
 #define ll long long
-#define MOD = 10e7;
 
 int main(){
     int n;
