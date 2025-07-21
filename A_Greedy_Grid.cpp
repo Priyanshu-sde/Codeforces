27e21d453ab2c4804d83a1d741b4e220a632a848
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-07-22 00:12:15 +0530

    refactor: simplify and optimize dynamic programming solution, add new macros and constants, modify recursive function and main logic, and remove unused variables and code.

--- A_Greedy_Grid.cpp
+++ A_Greedy_Grid.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+     
 #define fr(i,n) for(int  i = 0;i < n;i++)
 #define sr(a) sort(a.begin(),a.end())
 #define srd(a) sort(a.begin(),a.end(),greater<int>())
