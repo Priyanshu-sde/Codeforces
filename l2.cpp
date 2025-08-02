366634267c2b5c056042b20b86df3c4e43f72ca6
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-08-02 20:26:48 +0530

    a<filename> solution added

---
+++ l2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        int minRemoval(vector<int>& nums, int k) {\
+            sort(nums.begin(),nums.end());
+            int low = 0, high = nums.size() - 1;
+            for(int i = 0;i <=high;i++){
+                mp[nums[i]] = i;
+            }
+            int ans = 0;
+            for(int i = 0;i <= high;i++){
+                if(mp[a[i]*k] =)
+            }
+        }
+    };©leetcode
