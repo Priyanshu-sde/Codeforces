c1dc65f6d5cab918ae90da07e1de3b57157da605
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-08-02 21:26:50 +0530

    refactor(l2): Replace minRemoval with earliestFinishTime function

--- l2.cpp
+++ l2.cpp
@@ -1,14 +1,6 @@
 class Solution {
     public:
-        int minRemoval(vector<int>& nums, int k) {\
-            sort(nums.begin(),nums.end());
-            int low = 0, high = nums.size() - 1;
-            for(int i = 0;i <=high;i++){
-                mp[nums[i]] = i;
-            }
-            int ans = 0;
-            for(int i = 0;i <= high;i++){
-                if(mp[a[i]*k] =)
-            }
+        int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+            
         }
-    };©leetcode
+    };
