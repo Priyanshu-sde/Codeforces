a3eaa4f80aafd5a09bb4648e40bc32b91f3a801e
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-08-27 21:25:58 +0530

    aPermutation_Cost_Sum solution added

---
+++ Permutation_Cost_Sum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i,n) for(int  i = 0;i < n;i++)
+#define sr(a) sort(a.begin(),a.end())
+#define srd(a) sort(a.begin(),a.end(),greater<int>())
+#define ll long long
+const int N = 1e7;
+#define all(a) a.begin(),a.end()
+#define debug(x) cerr << #x << " = " << x << endl;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin>>t;
+    while(t--){
+        long long n;
+	    cin>>n;
+	    vector<long long> p(n);
+	    long long cnt = 0LL;
+	    long long cnt2 = 0LL;
+	    for(int i =0;i < n;i++){
+	        cin>>p[i];
+	        if(p[i] = i + 1) cnt++;
+	    }
+	    vector<bool> vis(n,false);
+	    for(int i = 0;i < n;i++){
+	        if(!vis[i]){
+	            int curr = i;
+	            while(p[curr] != i + 1 && !vis[curr] ){
+	                curr = p[curr] - 1;
+	                vis[curr] = true;
+	            }
+	            debug(i)
+	        }
+	    }
+	    long long ans = 0LL;
+	    long long rem = n -cnt;
+	    long long rem2 = n -cnt2;
+	    for(int i =0;i < n;i++){
+	        long long ni = n-i;
+	        long long mdf =(rem2)*((ni)*(ni+1)/2 - (i*(i+1))/2);
+	        long long swp = (rem*(i*(i+1))/2 );
+
+            debug(rem2)
+	        ans = (ans + mdf + swp);
+	    }
+	    cout<<ans<<endl;
+    }
+    return 0;
+}
