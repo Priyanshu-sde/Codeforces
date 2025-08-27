bc5cd442318b30e3cfac4201d55023a8900e444a
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-08-28 00:33:48 +0530

    aAlgorithms/new solution added

---
+++ Algorithms/new.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i, n) for (int i = 0; i < n; i++)
+#define sr(a) sort(a.begin(), a.end())
+#define srd(a) sort(a.begin(), a.end(), greater<int>())
+#define ll long long
+const int N = 1e7;
+#define all(a) a.begin(), a.end()
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    while (1)
+    {
+        if(!(cin>>t)) return 0;
+        for (int i = 1; i <= t; i++)
+            cout << i << endl;
+    }
+    return 0;
+}
