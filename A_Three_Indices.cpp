2875549014ac541989293f8b1572ca44a152078c
Priyanshu Chaurasia <priyanshu.sde@gmail.com>
2026-06-13 11:13:04 +0530

    refactor: improve code formatting and readability in A_Three_Indices.cpp

--- A_Three_Indices.cpp
+++ A_Three_Indices.cpp
@@ -1,37 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-#define sr(a) sort(a.begin(),a.end())
-#define srd(a) sort(a.begin(),a.end(),greater<int>())
+#define sr(a) sort(a.begin(), a.end())
+#define srd(a) sort(a.begin(), a.end(), greater<int>())
 #define ll long long
 #define MOD 10e7;
 
-int main(){
-    ios_base:: sync_with_stdio(false);
+int main()
+{
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int tc = 1;
-    cin>>tc;
-    while(tc--){
+    cin >> tc;
+    while (tc--)
+    {
         int n;
-        cin>>n;
+        cin >> n;
         vector<int> a(n);
-        for(int i = 0 ;i < n;i++){
-            cin>>a[i];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
         }
-        for(int i = 0 ;i < n;i++){
-            for(int j = i+1;j < n;j++){
-                for(int k = j+1;k < n;k++){
-                    if(a[i] < a[j] && a[j] > a[k]){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (a[i] < a[j])
+                {
+                    for (int k = j + 1; k < n; k++)
+                    {
+                        if(a[j] > a[k]){
                         cout<<"YES"<<endl<<i+1<<" "<<j+1<<" "<<k+1<<endl;
                         n = -1;
                         break;
                     }
-
+                    }
+                    break;
                 }
             }
         }
-        if(n != -1)
-        cout<<"NO"<<endl;
+        if (n != -1)
+            cout << "NO" << endl;
     }
 }
